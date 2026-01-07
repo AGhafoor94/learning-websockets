066458c7ac38faab11765d2f4cc25e1e46047c95
AGhafoor94 <adnan_ghafoor94@hotmail.com>
2026-01-07 17:20:25 +0000

    Updated styling

--- main.go
+++ main.go
@@ -1,22 +1,38 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
+)
 
-/*
-Try WebRTC
-
- WEBSOCKETS:
-
-		HTTP Header: Connection: Upgrade ->
-		<- HTTP 101 Switching Protocols
-	<- WebSocket Bidirectional Communication ->
-					Close ->
-*/
+const port string = ":8080"
 
 func main() {
-	setup_http_server()
+	fmt.Println("STARTED")
+	http.HandleFunc("/details-login", submit_login_details)
+	start_server()
 }
+func start_server() {
+	http.Handle("/", http.FileServer(http.Dir("./src/templates")))
+	cmd := exec.Command("powershell", "-Command", fmt.Sprintf(`[System.Diagnostics.Process]::Start("msedge", "http://localhost%s")`, port))
+	// http.HandleFunc("/details-login", submit_login_details)
+	_, err := cmd.Output()
 
-func setup_http_server() {
-	http.Handle("/", http.FileServer(http.Dir("./templates")))
+	if err != nil {
+		panic(err)
+	}
+	log.Fatal(http.ListenAndServe(port, nil))
+}
+func submit_login_details(response_writer http.ResponseWriter, request *http.Request) {
+
+	err := request.ParseForm()
+	if err != nil {
+		// Handle error here via logging and then return
+		fmt.Println(err)
+	}
+
+	name := request.PostFormValue("username")
+	fmt.Fprintf(response_writer, "Hello, %s!", name)
 }
