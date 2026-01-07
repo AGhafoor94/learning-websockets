a8331aefe1b17011fb6e69f29e1c27d637cbd742
AGhafoor94 <adnan_ghafoor94@hotmail.com>
2026-01-07 04:49:07 +0000

    Inital Push

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+/*
+Try WebRTC
+
+ WEBSOCKETS:
+
+		HTTP Header: Connection: Upgrade ->
+		<- HTTP 101 Switching Protocols
+	<- WebSocket Bidirectional Communication ->
+					Close ->
+*/
+
+func main() {
+}
