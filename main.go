8e1211ea8e26aabbd09bd7b67cdbcb21ed28b2af
AGhafoor94 <adnan_ghafoor94@hotmail.com>
2026-01-09 18:42:02 +0000

    Working on sending response back after data sent

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
-	"os/exec"
+
+	// "os/exec"
 
 	"github.com/gorilla/websocket"
 )
+
+type status_return_struct struct {
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+}
 
 var (
 	websocket_upgrader = websocket.Upgrader{
@@ -28,22 +36,41 @@ func start_server() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.Handle("/", http.FileServer(http.Dir("./templates")))
 	http.HandleFunc("/websocket", websocket_manager.serve_websocket)
-	cmd := exec.Command("powershell", "-Command", fmt.Sprintf(`[System.Diagnostics.Process]::Start("msedge", "http://localhost%s")`, port))
-	// http.HandleFunc("/details-login", submit_login_details)
-	_, err := cmd.Output()
+	// cmd := exec.Command("powershell", "-Command", fmt.Sprintf(`[System.Diagnostics.Process]::Start("chrome", "http://localhost%s")`, port))
+	// _, err := cmd.Output()
 
-	if err != nil {
-		panic(err)
-	}
+	// if err != nil {
+	// 	panic(err)
+	// }
 	log.Fatal(http.ListenAndServe(port, nil))
 }
+
 func submit_login_details(response_writer http.ResponseWriter, request *http.Request) {
-
-	err := request.ParseForm()
+	data_byte_array, err := io.ReadAll(request.Body)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Unable to read all request body: ", err)
 	}
+	fmt.Println(string(data_byte_array))
+	// err = request.ParseForm()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 
-	name := request.PostFormValue("username")
-	fmt.Fprintf(response_writer, "Hello, %s!", name)
+	// name := request.PostFormValue("username")
+	// fmt.Fprintf(response_writer, "Hello, %s!", name)
+	type mapped_test_data map[string]string
+	var new_data_temp = make(map[string]string)
+	new_data_temp["test"] = "TEST DATA"
+	new_data_temp["test_2"] = "TEST DATA_2"
+
+	json_string_to_send, err := json.Marshal(new_data_temp)
+
+	status_return := status_return_struct{
+		Message: string(json_string_to_send),
+		Status:  http.StatusOK,
+	}
+	response_writer.Header().Add("Content-Type", "application/json")
+	// http.StatusOK
+	response_writer.WriteHeader(http.StatusOK)
+	json.NewEncoder(response_writer).Encode(status_return)
 }
--- static/index.js
+++ static/index.js
@@ -1,216 +1,241 @@
 let show_side_bar = true;
-      let websocket_connection;
-      const sftp_connections = [
-        {
-          display_name: "Test",
-          ip: "10.200.:22",
-          user_access: true,
-        },
-        {
-          display_name: "Test 234",
-          ip: "10.300.:22",
-          user_access: true,
-        },
-      ];
-      window.addEventListener("load", (event) => {
-        // username_input = document.getElementById("username");
-        // password_input = document.getElementById("password");
-        const client_style = localStorage.getItem("client_style");
-        if (client_style === null) {
-          localStorage.setItem("client_style", "default");
-        }
-        document.getElementById("menu-opened").style.display = "block";
-        document.getElementById("menu-closed").style.display = "none";
-        document.getElementById("side-menu").style.width = "250px";
+let websocket_connection;
+const submit_user_button = document.getElementById("submit-user-button");
+const sftp_connections = [
+  {
+    display_name: "Test",
+    ip: "10.200.:22",
+    user_access: true,
+  },
+  {
+    display_name: "Test 234",
+    ip: "10.300.:22",
+    user_access: true,
+  },
+];
+window.addEventListener("load", (event) => {
+  // username_input = document.getElementById("username");
+  // password_input = document.getElementById("password");
+  const client_style = localStorage.getItem("client_style");
+  if (client_style === null) {
+    localStorage.setItem("client_style", "default");
+  }
+  document.getElementById("menu-opened").style.display = "block";
+  document.getElementById("menu-closed").style.display = "none";
+  document.getElementById("side-menu").style.width = "250px";
 
-        show_side_bar = !show_side_bar;
-        if (client_style === "default") {
-          set_dark_style = false;
-        } else {
-          set_dark_style = true;
-        }
-        change_css_styles();
-        validate_for_submitting();
-        add_sftp_connections_to_side_bar();
+  show_side_bar = !show_side_bar;
+  if (client_style === "default") {
+    set_dark_style = false;
+  } else {
+    set_dark_style = true;
+  }
+  change_css_styles();
+  validate_for_submitting();
+  add_sftp_connections_to_side_bar();
 
-        if (window["WebSocket"]) {
-          console.log("Websockets supported");
-          websocket_connection = new WebSocket(
-            "ws://" + document.location.host + "/websocket"
-          );
-          // check if websocket is waiting etc and display
-          websocket_connection.onerror = function (event) {
-            console.log("EVENT ERROR: ", event);
-          };
-          websocket_connection.onmessage = function (event) {
-            console.log("event");
-            console.log(event);
-            document.getElementById("websocket-message").innerHTML += `
+  if (window["WebSocket"]) {
+    console.log("Websockets supported");
+    websocket_connection = new WebSocket(
+      "ws://" + document.location.host + "/websocket"
+    );
+    // check if websocket is waiting etc and display
+    websocket_connection.onerror = function (event) {
+      console.log("EVENT ERROR: ", event);
+    };
+    websocket_connection.onmessage = function (event) {
+      console.log("event");
+      console.log(event);
+      document.getElementById("websocket-message").innerHTML += `
             <p><span>${event.data}</span></p>
             `;
-          };
-        } else {
-          console.log("Websockets not supported");
+    };
+  } else {
+    console.log("Websockets not supported");
+  }
+});
+document.getElementById("side-bar-toggle").addEventListener("click", () => {
+  if (show_side_bar) {
+    document.getElementById("menu-opened").style.display = "block";
+    document.getElementById("menu-closed").style.display = "none";
+    document.getElementById("side-menu").style.width = "250px";
+    document.getElementById("side-menu").style.display = "block";
+    show_side_bar = !show_side_bar;
+  } else {
+    document.getElementById("menu-closed").style.display = "block";
+    document.getElementById("menu-opened").style.display = "none";
+    document.getElementById("side-menu").style.display = "none";
+    show_side_bar = !show_side_bar;
+  }
+});
+function change_css_styles() {
+  console.log("Change styles clicked");
+  if (set_dark_style) {
+    document.getElementById("moon-svg").style.display = "none";
+    document.getElementById("sun-svg").style.display = "block";
+    let list_of_default_styles = document.querySelectorAll(".default");
+    list_of_default_styles.forEach((item) => {
+      // item.classList.remove("dark");
+      let current_default_class = "";
+      item.classList.forEach((default_class) => {
+        if (default_class.includes("default-")) {
+          current_default_class = default_class;
         }
       });
-      document
-        .getElementById("side-bar-toggle")
-        .addEventListener("click", () => {
-          if (show_side_bar) {
-            document.getElementById("menu-opened").style.display = "block";
-            document.getElementById("menu-closed").style.display = "none";
-            document.getElementById("side-menu").style.width = "250px";
-            document.getElementById("side-menu").style.display = "block";
-            show_side_bar = !show_side_bar;
-          } else {
-            document.getElementById("menu-closed").style.display = "block";
-            document.getElementById("menu-opened").style.display = "none";
-            document.getElementById("side-menu").style.display = "none";
-            show_side_bar = !show_side_bar;
-          }
-        });
-      function change_css_styles() {
-        console.log("Change styles clicked");
-        if (set_dark_style) {
-          document.getElementById("moon-svg").style.display = "none";
-          document.getElementById("sun-svg").style.display = "block";
-          let list_of_default_styles = document.querySelectorAll(".default");
-          list_of_default_styles.forEach((item) => {
-            // item.classList.remove("dark");
-            let current_default_class = "";
-            item.classList.forEach((default_class) => {
-              if (default_class.includes("default-")) {
-                current_default_class = default_class;
-              }
-            });
-            item.classList.remove("default");
-            item.classList.remove(current_default_class);
-            item.classList.add("dark");
-            item.classList.add(
-              current_default_class.replace("default", "dark")
-            );
-          });
-          localStorage.setItem("client_style", "dark");
-          set_dark_style = false;
-        } else {
-          document.getElementById("sun-svg").style.display = "none";
-          document.getElementById("moon-svg").style.display = "block";
-          let list_of_default_styles = document.querySelectorAll(".dark");
-          list_of_default_styles.forEach((item) => {
-            // item.classList.remove("dark");
-            let current_default_class = "";
-            item.classList.forEach((default_class) => {
-              if (default_class.includes("dark-")) {
-                current_default_class = default_class;
-              }
-            });
-            item.classList.remove("dark");
-            item.classList.remove(current_default_class);
-            item.classList.add("default");
-            item.classList.add(
-              current_default_class.replace("dark", "default")
-            );
-          });
-          localStorage.setItem("client_style", "default");
-          set_dark_style = true;
+      item.classList.remove("default");
+      item.classList.remove(current_default_class);
+      item.classList.add("dark");
+      item.classList.add(current_default_class.replace("default", "dark"));
+    });
+    localStorage.setItem("client_style", "dark");
+    set_dark_style = false;
+  } else {
+    document.getElementById("sun-svg").style.display = "none";
+    document.getElementById("moon-svg").style.display = "block";
+    let list_of_default_styles = document.querySelectorAll(".dark");
+    list_of_default_styles.forEach((item) => {
+      // item.classList.remove("dark");
+      let current_default_class = "";
+      item.classList.forEach((default_class) => {
+        if (default_class.includes("dark-")) {
+          current_default_class = default_class;
         }
-      }
-      let set_dark_style = true;
-
-      let selected_ip = sftp_connections[0].ip;
-      const change_ip = () => {
-        let new_selected_ip = document.getElementById(`button-${1}`).value;
-        if (new_selected_ip !== selected_ip) {
-          console.log(new_selected_ip);
-        }
-        return false;
-      };
-
-      enable_connect_button = 0;
-      username_entered = false;
-      password_entered = false;
-      document.getElementById("username").addEventListener("input", (event) => {
-        if (event.target.value.length > 0) {
-          username_entered = true;
-        } else {
-          username_entered = false;
-        }
-        console.log(event.target.value);
-
-        validate_for_submitting();
       });
-      document.getElementById("password").addEventListener("input", (event) => {
-        if (event.target.value.length > 0) {
-          password_entered = true;
-        } else {
-          password_entered = false;
-        }
-        console.log(event.target.value);
-        validate_for_submitting();
-      });
-      function add_sftp_connections_to_side_bar() {
-        sftp_side_menu = document.getElementById("side-menu");
-        sftp_connections.forEach((item, index) => {
-          sftp_side_menu.innerHTML += `
+      item.classList.remove("dark");
+      item.classList.remove(current_default_class);
+      item.classList.add("default");
+      item.classList.add(current_default_class.replace("dark", "default"));
+    });
+    localStorage.setItem("client_style", "default");
+    set_dark_style = true;
+  }
+}
+let set_dark_style = true;
+
+let selected_ip = sftp_connections[0].ip;
+const change_ip = () => {
+  let new_selected_ip = document.getElementById(`button-${1}`).value;
+  if (new_selected_ip !== selected_ip) {
+    console.log(new_selected_ip);
+  }
+  return false;
+};
+
+enable_connect_button = 0;
+username_entered = false;
+password_entered = false;
+document.getElementById("username").addEventListener("input", (event) => {
+  if (event.target.value.length > 0) {
+    username_entered = true;
+  } else {
+    username_entered = false;
+  }
+  console.log(event.target.value);
+
+  validate_for_submitting();
+});
+document.getElementById("password").addEventListener("input", (event) => {
+  if (event.target.value.length > 0) {
+    password_entered = true;
+  } else {
+    password_entered = false;
+  }
+  console.log(event.target.value);
+  validate_for_submitting();
+});
+
+submit_user_button.addEventListener("click", () => {
+  post_data_to_endpoint();
+});
+
+function add_sftp_connections_to_side_bar() {
+  sftp_side_menu = document.getElementById("side-menu");
+  sftp_connections.forEach((item, index) => {
+    sftp_side_menu.innerHTML += `
             <button id='button-${index}' class='button' style='width: 100%; padding: 5px;margin: 5px 0' onclick='set_ip_address(${index})'>${item.display_name}</button>
           `;
-        });
-      }
-      function set_ip_address(target_id) {
-        console.log(sftp_connections[target_id]);
-        console.log(target_id);
-        document.getElementById("ip-chosen").innerHTML =
-          sftp_connections[target_id].ip;
-        document.getElementById("ip-input").value =
-          sftp_connections[target_id].ip;
-        validate_for_submitting();
-      }
-      function validate_for_submitting() {
-        submit_user_button = document.getElementById("submit-user-button");
-        if (
-          username_entered &&
-          password_entered &&
-          document.getElementById("ip-input").value !== ""
-        ) {
-          submit_user_button.removeAttribute("disabled");
-        } else {
-          let validation_error = "";
-          submit_user_button.setAttribute("disabled", "true");
-          if (!username_entered) {
-            validation_error += " Please enter username. ";
-          }
-          if (!password_entered) {
-            validation_error += " Please enter password. ";
-          }
-          if (document.getElementById("ip-input").value === "") {
-            validation_error += " Please select SFTP Server. ";
-          }
-        }
-      }
-      function post_data_to_endpoint() {
-        var xhr = new XMLHttpRequest();
-        console.log("HERE");
-        xhr.open("POST", "https://localhost:8080/details-login", true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(
-          JSON.stringify({
-            test: "Test",
-          })
-        );
-      }
+  });
+}
+function set_ip_address(target_id) {
+  console.log(sftp_connections[target_id]);
+  console.log(target_id);
+  document.getElementById("ip-chosen").innerHTML =
+    sftp_connections[target_id].ip;
+  //   document.getElementById("ip-input").value = sftp_connections[target_id].ip;
 
-      function send_message() {
-        let username = document.getElementById("username");
-        if (username.value !== null) {
-          websocket_connection.send(username.value);
-        }
-        return false;
-      }
+  validate_for_submitting();
+}
+function validate_for_submitting() {
+  if (
+    username_entered &&
+    password_entered
+    // && document.getElementById("ip-input").value !== ""
+  ) {
+    submit_user_button.removeAttribute("disabled");
+  } else {
+    let validation_error = "";
+    submit_user_button.setAttribute("disabled", "true");
+    if (!username_entered) {
+      validation_error += " Please enter username. ";
+    }
+    if (!password_entered) {
+      validation_error += " Please enter password. ";
+    }
+    // if (document.getElementById("ip-input").value === "") {
+    //   validation_error += " Please select SFTP Server. ";
+    // }
+  }
+}
 
-      function read_message() {
-        let password = document.getElementById("password");
-        if (username.value !== null) {
-          websocket_connection.get();
-        }
-        return false;
+function post_data_to_endpoint() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://localhost:8080/details-login", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  // xhr.send(
+  //   JSON.stringify({
+  //     test: "Test",
+  //   })
+  // );
+  console.log(xhr.getAllResponseHeaders());
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      // If the request is successful, parse the JSON response
+      try {
+        var responseJson = JSON.parse(xhr.responseText); // Parse JSON string to an object
+
+        // Handle the response (e.g., display it in a div)
+        document.getElementById(
+          "responseOutput"
+        ).innerHTML = `<strong>Message:</strong> ${responseJson.message}<br><strong>Status:</strong> ${responseJson.status}`;
+      } catch (error) {
+        console.error("Error parsing JSON:", error);
       }
+    } else {
+      // If the request fails, log the error
+      console.error("Request failed with status:", xhr.status);
+    }
+  };
+  xhr.send(
+    JSON.stringify({
+      username: document.getElementById("username").value,
+      password: document.getElementById("password").value,
+      ip: document.getElementById("ip-chosen").innerHTML,
+    })
+  );
+}
+
+function send_message() {
+  let username = document.getElementById("username");
+  if (username.value !== null) {
+    websocket_connection.send(username.value);
+  }
+  return false;
+}
+
+function read_message() {
+  let password = document.getElementById("password");
+  if (username.value !== null) {
+    websocket_connection.get();
+  }
+  return false;
+}
